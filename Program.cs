af3e7c707027492793cf5a77a0e75b1219238e90
apazala <albertopascual@hotmail.es>
2022-11-08 18:35:04 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double sumd = 0.0;
+
+        const Int32 BufferSize = 65536;
+        string fileName = "data.txt";
+        using (var fileStream = File.OpenRead(fileName))
+        {
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    double b = Double.Parse(line);
+                    sumd += b;
+                }
+            }
+        }
+
+        double lim = 1e10;
+        while(sumd > lim)
+            sumd*=.1;
+
+        Console.WriteLine((long)sumd);
+
+    }
+}
